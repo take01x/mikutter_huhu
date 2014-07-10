3ae7cf2aaeae8b093ec3b8a69ab3d6183cb54b9d
Takehiro YOSHIHAMA <take001x@gmail.com>
2014-07-10 13:36:15 +0900

    複数選択に対応

--- mikutter_huhu.rb
+++ mikutter_huhu.rb
@@ -8,11 +8,13 @@ Plugin.create(:mikutter_huhu) do
           visible: true,
           role: :timeline
           ) do |opt|
-    user_name = opt.messages[0].user.idname
-    message = "@" + user_name + " ふふっ(*^_^*)"
-    message += '　' * (rand(140-message.split(//).size+1)+1)
-    Service.primary.post(:message => message,
-                         :replyto => opt.messages[0])
+    opt.messages.map{ |m|
+      user_name = m.message.user.idname
+      message = "@" + user_name + " ふふっ(*^_^*)"
+      message += '　' * (rand(140-message.split(//).size+1)+1)
+      Service.primary.post(:message => message,
+                           :replyto => m.message)
+    }
   end
 end
 
